4db484cbb3610cf0735b85ec06faa418c0a2cc4b
g.bon <g.bon@student.isartdigital.com>
2025-02-02 17:57:01 +0100

    reset project

---
+++ MotionCore/include/core/motion_core_engine.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "motion_core_header.h"
+
+class MotionCoreEngine
+{
+public:
+	MOTION_CORE MotionCoreEngine();
+
+	MOTION_CORE ~MotionCoreEngine() = default;
+};
+
---
+++ MotionCore/source/core/motion_core_engine.cpp
@@ -0,0 +1,9 @@
+#include "core/motion_core_engine.hpp"
+
+#include <iostream>
+
+
+MotionCoreEngine::MotionCoreEngine()
+{
+	std::cout << "MotionCoreEngine " << std::endl;
+}
