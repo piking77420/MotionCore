6215fbba2b31fab4e15c51a31c9ae6a990c71080
piking <bon.gabriel77@gmail.com>
2025-03-30 17:32:49 +0200

    Modify common headers

--- MotionCore/src/include/math/toolbox.hpp
+++ MotionCore/src/include/math/toolbox.hpp
@@ -1,19 +1,3 @@
 ﻿#pragma once
-
-
-#if defined(MOTION_DYNAMIC_IMAGE)
-#ifdef MOTION_CORE_EXPORTS
-#define MOTION_CORE_API __declspec(dllexport)
-#elif MOTION_CORE_IMPORTS
-#define MOTION_CORE_API __declspec(dllimport)
-#else
-#define MOTION_CORE_API
-#endif
-#else
-#define MOTION_CORE_API
-#endif
-
-#define FORCEINLINE __forceinline
-
 #include "toolbox_typedef.hpp"
 #include "matrix_transformation.hpp"
--- MotionCore/src/include/math/toolbox_headerfile.hpp
+++ MotionCore/src/include/math/toolbox_headerfile.hpp
@@ -2,11 +2,36 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <cmath>
-#include <array>
-#include <string>
+#include <stdint.h>
+#include <cassert>
 
-#define TOOLBOX_INLINE _forceinline 
+#include "simd_define.h"
+
+#if defined(MOTION_DYNAMIC_IMAGE)
+#ifdef MOTION_CORE_EXPORTS
+#define MOTION_CORE_API __declspec(dllexport)
+#elif MOTION_CORE_IMPORTS
+#define MOTION_CORE_API __declspec(dllimport)
+#else
+#define MOTION_CORE_API
+#endif
+#else
+#define MOTION_CORE_API
+#endif
+
+#define FORCEINLINE __forceinline
 #define EPSILON 1e-6
+
+#if defined(__clang__)
+#define RESTRICT __restrict
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+#define RESTRICT __restrict__
+
+#elif defined(_MSC_VER)
+#define RESTRICT __restrict
+
+#endif
 
 static constexpr float fEpsilon = 1e-6f;
 static constexpr double dEpsilon = 1e-6;
@@ -17,5 +42,8 @@ constexpr bool IsEqual(T _value, T _compare, T epsilon = static_cast<T>(EPSILON)
     return std::abs(_value - _compare) <= epsilon;
 }
 
-constexpr float Deg2Rad { static_cast<float>(M_PI) / 180.0f};
-constexpr float Rad2Deg {57.29578f};
+constexpr float fDeg2Rad { static_cast<float>(M_PI) / 180.0f};
+constexpr float fRad2Deg {57.29578f};
+
+constexpr double dDeg2Rad{ static_cast<double>(M_PI) / 180.0 };
+constexpr double dRad2Deg{ 57.29578 };
