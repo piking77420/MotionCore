034ec1e576108a50ec10a4127e9d6e7e916c45e2
piking77420 <bon.gabriel77@gmail.com>
2025-05-02 11:38:52 +0200

    forceinline for other compiler

--- MotionCore/src/include/math/toolbox_headerfile.hpp
+++ MotionCore/src/include/math/toolbox_headerfile.hpp
@@ -18,7 +18,17 @@
 #define MOTION_CORE_API
 #endif
 
+#ifndef FORCEINLINE
+#if defined(_MSC_VER)
 #define FORCEINLINE __forceinline
+#elif defined(__GNUC__) || defined(__clang__)
+#define FORCEINLINE inline __attribute__((always_inline))
+#else
+#define FORCEINLINE inline
+#endif
+#endif
+
+#define INLINE inline
 
 #if defined(__clang__)
 #define RESTRICT __restrict
