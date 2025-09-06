235b493a0dd16abc48956773b76f88fd24fb9f49
piking77420 <bon.gabriel77@gmail.com>
2025-09-06 11:35:24 +0200

    Remove Auto Simd
    The user shoudle undle it by itself.
    For exemple build cross plateform project
    the local device instruction set is not the wanted one.

--- UnitTest/vector3_test.cpp
+++ UnitTest/vector3_test.cpp
@@ -2,35 +2,32 @@
 
 #include "math/toolbox_typedef.hpp"
 
+#include <immintrin.h>
+
+
 using namespace Tbx;
 
 TEST(TestVec3, Constructor)
 {
-	Vector3<float> v = Vector3<float>(1.f, 2.f, 3.f);
-
-
-	EXPECT_TRUE(IsEqual(v.x, 1.f));
-	EXPECT_TRUE(IsEqual(v.y, 2.f));
-	EXPECT_TRUE(IsEqual(v.z, 3.f));
-
+	Vector3<double> v = Vector3<double>(1., 2., 3.);
 }
 
 TEST(TestVec3, Magnitude)
 {
-	Vector3<float> v = Vector3<float>(1.f, 2.f, 3.f);
+	Vector3<double> v = Vector3<double>(1., 2., 3.);
 
-	float mag = v.Magnitude();
+	double mag = v.Magnitude();
 
-	EXPECT_TRUE(IsEqual(mag, 3.74165750f));
+	EXPECT_TRUE(IsEqual(mag, 3.74165750));
 
 
 }
 
 TEST(TestVec3, Normalize)
 {
-	Vector3<float> v = Vector3<float>(1.f, 0.f, 0.f);
+	Vector3<double> v = Vector3<double>(1., 0., 0.);
 
-	EXPECT_TRUE(IsEqual(v.Magnitude(), 1.f));
+	EXPECT_TRUE(IsEqual(v.Magnitude(), 1.));
 
 }
 
@@ -38,14 +35,14 @@ TEST(TestVec3, Normalize)
 TEST(TestVec3, Distance)
 {
 	{
-		Vector3<float> v = Vector3<float>(1.f, 0.f, 0.f);
+		Vector3<double> v = Vector3<double>(1., 0., 0.);
 
-		Vector3<float> v1 = Vector3<float>(0.f, 1.f, 0.f);
+		Vector3<double> v1 = Vector3<double>(0., 1., 0.);
 
-		float distance = Vector3<float>::DistanceSquare(v, v1);
+		double distance = Vector3<double>::DistanceSquare(v, v1);
 
-		EXPECT_FLOAT_EQ(distance, 2.f);
-		EXPECT_FLOAT_EQ(Vector3<float>::Distance(v, v1), std::sqrt(2.f));
+		EXPECT_FLOAT_EQ(distance, 2.);
+		EXPECT_FLOAT_EQ(Vector3<double>::Distance(v, v1), std::sqrt(2.));
 
 	}
 	
@@ -56,7 +53,7 @@ TEST(TestVec3, Distance)
 
 		double distance = Vector3<double>::DistanceSquare(v, v1);
 
-		EXPECT_FLOAT_EQ(distance, 2.f);
+		EXPECT_FLOAT_EQ(distance, 2.);
 		EXPECT_FLOAT_EQ(Vector3<double>::Distance(v, v1), std::sqrt(2.));
 	}
 
@@ -66,26 +63,26 @@ TEST(TestVec3, Distance)
 
 TEST(TestVec3, Dot)
 {
-	float v = Vector3<float>::Dot(Vector3<float>::UnitX(), Vector3<float>::UnitY());
-	EXPECT_TRUE(IsEqual(v, 0.f));
+	double v = Vector3<double>::Dot(Vector3<double>::UnitX(), Vector3<double>::UnitY());
+	EXPECT_TRUE(IsEqual(v, 0.));
 
-	v = Vector3<float>::Dot(Vector3<float>::UnitX(), Vector3<float>::UnitZ());
-	EXPECT_TRUE(IsEqual(v, 0.f));
+	v = Vector3<double>::Dot(Vector3<double>::UnitX(), Vector3<double>::UnitZ());
+	EXPECT_TRUE(IsEqual(v, 0.));
 
-	v = Vector3<float>::Dot(Vector3<float>::UnitX(), Vector3<float>::UnitX());
-	EXPECT_TRUE(IsEqual(v, 1.f));
+	v = Vector3<double>::Dot(Vector3<double>::UnitX(), Vector3<double>::UnitX());
+	EXPECT_TRUE(IsEqual(v, 1.));
 }
 
 TEST(TestVec3, Cross)
 {
 	{
-		Vector3<float> v = Vector3<float>::UnitX();
+		Vector3<double> v = Vector3<double>::UnitX();
 
-		Vector3<float> v1 = Vector3<float>::UnitY();
+		Vector3<double> v1 = Vector3<double>::UnitY();
 
-		Vector3<float> unitZ = Vector3<float>::Cross(Vector3<float>::UnitX(), Vector3<float>::UnitY());
+		Vector3<double> unitZ = Vector3<double>::Cross(Vector3<double>::UnitX(), Vector3<double>::UnitY());
 
-		EXPECT_TRUE(unitZ == Vector3<float>::UnitZ());
+		EXPECT_TRUE(unitZ == Vector3<double>::UnitZ());
 	}
 
 	{
@@ -105,10 +102,10 @@ TEST(TestVec3, Cross)
 TEST(TestVec3, operatorAdd)
 {
 	{
-		Vector3<float> v = Vector3<float>::UnitX();
-		Vector3<float> v2 = Vector3<float>::UnitY();
+		Vector3<double> v = Vector3<double>::UnitX();
+		Vector3<double> v2 = Vector3<double>::UnitY();
 
-		Vector3<float> expectResult = Vector3<float>(1.f, 1.f, 0.f);
+		Vector3<double> expectResult = Vector3<double>(1., 1., 0.);
 
 		EXPECT_TRUE(expectResult == v + v2);
 	}
@@ -126,10 +123,10 @@ TEST(TestVec3, operatorAdd)
 TEST(TestVec3, operatorSub)
 {
 	{
-		Vector3<float> v = Vector3<float>::UnitX();
-		Vector3<float> v2 = Vector3<float>::UnitY();
+		Vector3<double> v = Vector3<double>::UnitX();
+		Vector3<double> v2 = Vector3<double>::UnitY();
 
-		Vector3<float> expectResult = Vector3<float>(1.f, -1.f, 0.f);
+		Vector3<double> expectResult = Vector3<double>(1., -1., 0.);
 
 		EXPECT_TRUE(expectResult == v - v2);
 	}
@@ -147,10 +144,10 @@ TEST(TestVec3, operatorSub)
 TEST(TestVec3, operatorScalarAdd)
 {
 	{
-		Vector3<float> v = Vector3<float>::UnitX();
-		float scalar = 2.f;
+		Vector3<double> v = Vector3<double>::UnitX();
+		double scalar = 2.;
 
-		Vector3<float> expectResult = Vector3<float>(3.f, 2.f, 2.f);
+		Vector3<double> expectResult = Vector3<double>(3., 2., 2.);
 
 		EXPECT_TRUE(expectResult == v + scalar);
 	}
@@ -169,10 +166,10 @@ TEST(TestVec3, operatorScalarAdd)
 TEST(TestVec3, operatorScalarSub)
 {
 	{
-		Vector3<float> v = Vector3<float>::UnitX();
-		float scalar = 2.f;
+		Vector3<double> v = Vector3<double>::UnitX();
+		double scalar = 2.;
 
-		Vector3<float> expectResult = Vector3<float>(-1.f, -2.f, -2.f);
+		Vector3<double> expectResult = Vector3<double>(-1., -2., -2.);
 
 		EXPECT_TRUE(expectResult == v - scalar);
 	}
@@ -191,10 +188,10 @@ TEST(TestVec3, operatorScalarSub)
 TEST(TestVec3, operatorScalarMul)
 {
 	{
-		Vector3<float> v = Vector3<float>::UnitX();
-		float scalar = 2.f;
+		Vector3<double> v = Vector3<double>::UnitX();
+		double scalar = 2.;
 
-		Vector3<float> expectResult = Vector3<float>(2.f, 0.f, 0.f);
+		Vector3<double> expectResult = Vector3<double>(2., 0., 0.);
 
 		EXPECT_TRUE(expectResult == v * scalar);
 	}
@@ -214,10 +211,10 @@ TEST(TestVec3, operatorScalarMul)
 TEST(TestVec3, operatorScalarDiv)
 {
 	{
-		Vector3<float> v = Vector3<float>::UnitX();
-		float scalar = 2.f;
+		Vector3<double> v = Vector3<double>::UnitX();
+		double scalar = 2.;
 
-		Vector3<float> expectResult = Vector3<float>(0.5f, 0.f, 0.f);
+		Vector3<double> expectResult = Vector3<double>(0.5f, 0., 0.);
 
 		EXPECT_TRUE(expectResult == v / scalar);
 	}
@@ -235,28 +232,28 @@ TEST(TestVec3, operatorScalarDiv)
 
 TEST(TestVec3, operatorBracket)
 {
-	Vector3<float> vec = Vector3<float>(1.f, 1.f, 1.f);
+	Vector3<double> vec = Vector3<double>(1., 1., 1.);
 
 	uint8_t* ptr = reinterpret_cast<uint8_t*>(&vec.x);
 
 	EXPECT_TRUE(ptr == (uint8_t*)&vec[0]);
-	EXPECT_TRUE(IsEqual(*(float*)ptr, vec[0]));
-	ptr += sizeof(float);
+	EXPECT_TRUE(IsEqual(*(double*)ptr, vec[0]));
+	ptr += sizeof(double);
 
 	EXPECT_TRUE(ptr == (uint8_t*)&vec[1]);
-	EXPECT_TRUE(IsEqual(*(float*)ptr, vec[1]));
-	ptr += sizeof(float);
+	EXPECT_TRUE(IsEqual(*(double*)ptr, vec[1]));
+	ptr += sizeof(double);
 
 	EXPECT_TRUE(ptr == (uint8_t*)&vec[2]);
-	EXPECT_TRUE(IsEqual(*(float*)ptr, vec[2]));
+	EXPECT_TRUE(IsEqual(*(double*)ptr, vec[2]));
 
 }
 
 TEST(TestVec3, operatorEqual)
 {
-	EXPECT_TRUE(Vector3<float>::UnitX() == Vector3<float>::UnitX());
-	EXPECT_FALSE(Vector3<float>::UnitX() == Vector3<float>::UnitY());
+	EXPECT_TRUE(Vector3<double>::UnitX() == Vector3<double>::UnitX());
+	EXPECT_FALSE(Vector3<double>::UnitX() == Vector3<double>::UnitY());
 
-	EXPECT_TRUE(Vector3<float>::UnitX() != Vector3<float>::UnitZ());
-	EXPECT_FALSE(Vector3<float>::UnitX() != Vector3<float>::UnitX());
+	EXPECT_TRUE(Vector3<double>::UnitX() != Vector3<double>::UnitZ());
+	EXPECT_FALSE(Vector3<double>::UnitX() != Vector3<double>::UnitX());
 }
--- UnitTest/vector4_test.cpp
+++ UnitTest/vector4_test.cpp
@@ -8,33 +8,33 @@ using namespace Tbx;
 
 TEST(TestVec4, Constructor)
 {
-	Vector4<float> v = Vector4<float>(1.f, 2.f, 3.f, 4.f);
+	Vector4<double> v = Vector4<double>(1., 2., 3., 4.);
 
 
-	EXPECT_TRUE(IsEqual(v.x, 1.f));
-	EXPECT_TRUE(IsEqual(v.y, 2.f));
-	EXPECT_TRUE(IsEqual(v.z, 3.f));
-	EXPECT_TRUE(IsEqual(v.w, 4.f));
+	EXPECT_TRUE(IsEqual(v.x, 1.));
+	EXPECT_TRUE(IsEqual(v.y, 2.));
+	EXPECT_TRUE(IsEqual(v.z, 3.));
+	EXPECT_TRUE(IsEqual(v.w, 4.));
 }
 
 TEST(TestVec4, Magnitude)
 {
-	Vector4<float> v = Vector4<float>(1.f, 2.f, 3.f, 4.f);
+	Vector4<double> v = Vector4<double>(1., 2., 3., 4.);
 
-	float magSquare = v.MagnitudeSquare();
+	double magSquare = v.MagnitudeSquare();
 
-	EXPECT_FLOAT_EQ(magSquare, 30.f);
+	EXPECT_FLOAT_EQ(magSquare, 30.);
 
-	float mag = v.Magnitude();
+	double mag = v.Magnitude();
 
 	EXPECT_FLOAT_EQ(mag ,std::sqrt(magSquare));
 }
 
 TEST(TestVec4, Normalize)
 {
-	Vector4<float> v = Vector4<float>(1.f, 0.f, 0.f, 0.f);
+	Vector4<double> v = Vector4<double>(1., 0., 0., 0.);
 
-	EXPECT_TRUE(IsEqual(v.Magnitude(), 1.f));
+	EXPECT_TRUE(IsEqual(v.Magnitude(), 1.));
 
 }
 
@@ -42,14 +42,14 @@ TEST(TestVec4, Normalize)
 TEST(TestVec4, Distance)
 {
 	{
-		Vector4<float> v = Vector4<float>(1.f, 0.f, 0.f, 0.f);
+		Vector4<double> v = Vector4<double>(1., 0., 0., 0.);
 
-		Vector4<float> v1 = Vector4<float>(0.f, 1.f, 0.f, 0.f);
+		Vector4<double> v1 = Vector4<double>(0., 1., 0., 0.);
 
-		float distance = Vector4<float>::DistanceSquare(v, v1);
+		double distance = Vector4<double>::DistanceSquare(v, v1);
 
-		EXPECT_FLOAT_EQ(distance, 2.f);
-		EXPECT_FLOAT_EQ(Vector4<float>::Distance(v, v1), std::sqrt(2.f));
+		EXPECT_FLOAT_EQ(distance, 2.);
+		EXPECT_FLOAT_EQ(Vector4<double>::Distance(v, v1), std::sqrt(2.));
 
 	}
 
@@ -60,7 +60,7 @@ TEST(TestVec4, Distance)
 
 		double distance = Vector4<double>::DistanceSquare(v, v1);
 
-		EXPECT_FLOAT_EQ(distance, 2.f);
+		EXPECT_FLOAT_EQ(distance, 2.);
 		EXPECT_FLOAT_EQ(Vector4<double>::Distance(v, v1), std::sqrt(2.));
 	}
 
@@ -70,17 +70,17 @@ TEST(TestVec4, Distance)
 
 TEST(TestVec4, Dot)
 {
-	float v = Vector4<float>::Dot(Vector4<float>::UnitX(), Vector4<float>::UnitY());
-	EXPECT_TRUE(IsEqual(v, 0.f));
+	double v = Vector4<double>::Dot(Vector4<double>::UnitX(), Vector4<double>::UnitY());
+	EXPECT_TRUE(IsEqual(v, 0.));
 
-	v = Vector4<float>::Dot(Vector4<float>::UnitX(), Vector4<float>::UnitZ());
-	EXPECT_TRUE(IsEqual(v, 0.f));
+	v = Vector4<double>::Dot(Vector4<double>::UnitX(), Vector4<double>::UnitZ());
+	EXPECT_TRUE(IsEqual(v, 0.));
 
-	v = Vector4<float>::Dot(Vector4<float>::UnitX(), Vector4<float>::UnitW());
-	EXPECT_TRUE(IsEqual(v, 0.f));
+	v = Vector4<double>::Dot(Vector4<double>::UnitX(), Vector4<double>::UnitW());
+	EXPECT_TRUE(IsEqual(v, 0.));
 
-	v = Vector4<float>::Dot(Vector4<float>::UnitX(), Vector4<float>::UnitX());
-	EXPECT_TRUE(IsEqual(v, 1.f));
+	v = Vector4<double>::Dot(Vector4<double>::UnitX(), Vector4<double>::UnitX());
+	EXPECT_TRUE(IsEqual(v, 1.));
 
 }
 
@@ -89,10 +89,10 @@ TEST(TestVec4, Dot)
 TEST(TestVec4, operatorAdd)
 {
 	{
-		Vector4<float> v = Vector4<float>::UnitX();
-		Vector4<float> v2 = Vector4<float>::UnitY();
+		Vector4<double> v = Vector4<double>::UnitX();
+		Vector4<double> v2 = Vector4<double>::UnitY();
 
-		Vector4<float> expectResult = Vector4<float>(1.f, 1.f, 0.f, 0.f);
+		Vector4<double> expectResult = Vector4<double>(1., 1., 0., 0.);
 
 		EXPECT_TRUE(expectResult == v + v2);
 	}
@@ -110,10 +110,10 @@ TEST(TestVec4, operatorAdd)
 TEST(TestVec4, operatorSub)
 {
 	{
-		Vector4<float> v = Vector4<float>::UnitX();
-		Vector4<float> v2 = Vector4<float>::UnitY();
+		Vector4<double> v = Vector4<double>::UnitX();
+		Vector4<double> v2 = Vector4<double>::UnitY();
 
-		Vector4<float> expectResult = Vector4<float>(1.f, -1.f, 0.f, 0.f);
+		Vector4<double> expectResult = Vector4<double>(1., -1., 0., 0.);
 
 		EXPECT_TRUE(expectResult == v - v2);
 	}
@@ -131,10 +131,10 @@ TEST(TestVec4, operatorSub)
 TEST(TestVec4, operatorScalarAdd)
 {
 	{
-		Vector4<float> v = Vector4<float>::UnitX();
-		float scalar = 2.f;
+		Vector4<double> v = Vector4<double>::UnitX();
+		double scalar = 2.;
 
-		Vector4<float> expectResult = Vector4<float>(3.f, 2.f, 2.f, 2.f);
+		Vector4<double> expectResult = Vector4<double>(3., 2., 2., 2.);
 
 		EXPECT_TRUE(expectResult == v + scalar);
 	}
@@ -153,10 +153,10 @@ TEST(TestVec4, operatorScalarAdd)
 TEST(TestVec4, operatorScalarSub)
 {
 	{
-		Vector4<float> v = Vector4<float>::UnitX();
-		float scalar = 2.f;
+		Vector4<double> v = Vector4<double>::UnitX();
+		double scalar = 2.;
 
-		Vector4<float> expectResult = Vector4<float>(-1.f, -2.f, -2.f, -2.f);
+		Vector4<double> expectResult = Vector4<double>(-1., -2., -2., -2.);
 
 		EXPECT_TRUE(expectResult == v - scalar);
 	}
@@ -175,10 +175,10 @@ TEST(TestVec4, operatorScalarSub)
 TEST(TestVec4, operatorScalarMul)
 {
 	{
-		Vector4<float> v = Vector4<float>::UnitX();
-		float scalar = 2.f;
+		Vector4<double> v = Vector4<double>::UnitX();
+		double scalar = 2.;
 
-		Vector4<float> expectResult = Vector4<float>(2.f, 0.f, 0.f, 0.f);
+		Vector4<double> expectResult = Vector4<double>(2., 0., 0., 0.);
 
 		EXPECT_TRUE(expectResult == v * scalar);
 	}
@@ -198,10 +198,10 @@ TEST(TestVec4, operatorScalarMul)
 TEST(TestVec4, operatorScalarDiv)
 {
 	{
-		Vector4<float> v = Vector4<float>::UnitX();
-		float scalar = 2.f;
+		Vector4<double> v = Vector4<double>::UnitX();
+		double scalar = 2.;
 
-		Vector4<float> expectResult = Vector4<float>(0.5f, 0.f, 0.f, 0.f);
+		Vector4<double> expectResult = Vector4<double>(0.5f, 0., 0., 0.);
 
 		EXPECT_TRUE(expectResult == v / scalar);
 	}
@@ -219,31 +219,31 @@ TEST(TestVec4, operatorScalarDiv)
 
 TEST(TestVec4, operatorBracket)
 {
-	Vector4<float> vec = Vector4<float>(1.f, 1.f, 1.f, 1.f);
+	Vector4<double> vec = Vector4<double>(1., 1., 1., 1.);
 
 	uint8_t* ptr = reinterpret_cast<uint8_t*>(&vec.x);
 
 	EXPECT_TRUE(ptr == (uint8_t*)&vec[0]);
-	EXPECT_TRUE(IsEqual(*(float*)ptr, vec[0]));
-	ptr += sizeof(float);
+	EXPECT_TRUE(IsEqual(*(double*)ptr, vec[0]));
+	ptr += sizeof(double);
 
 	EXPECT_TRUE(ptr == (uint8_t*)&vec[1]);
-	EXPECT_TRUE(IsEqual(*(float*)ptr, vec[1]));
-	ptr += sizeof(float);
+	EXPECT_TRUE(IsEqual(*(double*)ptr, vec[1]));
+	ptr += sizeof(double);
 
 	EXPECT_TRUE(ptr == (uint8_t*)&vec[2]);
-	EXPECT_TRUE(IsEqual(*(float*)ptr, vec[2]));
-	ptr += sizeof(float);
+	EXPECT_TRUE(IsEqual(*(double*)ptr, vec[2]));
+	ptr += sizeof(double);
 
 	EXPECT_TRUE(ptr == (uint8_t*)&vec[3]);
-	EXPECT_TRUE(IsEqual(*(float*)ptr, vec[3]));
+	EXPECT_TRUE(IsEqual(*(double*)ptr, vec[3]));
 }
 
 TEST(TestVec4, operatorEqual)
 {
-	EXPECT_TRUE(Vector4<float>::UnitX() == Vector4<float>::UnitX());
-	EXPECT_FALSE(Vector4<float>::UnitX() == Vector4<float>::UnitY());
+	EXPECT_TRUE(Vector4<double>::UnitX() == Vector4<double>::UnitX());
+	EXPECT_FALSE(Vector4<double>::UnitX() == Vector4<double>::UnitY());
 
-	EXPECT_TRUE(Vector4<float>::UnitX() != Vector4<float>::UnitZ());
-	EXPECT_FALSE(Vector4<float>::UnitX() != Vector4<float>::UnitX());
+	EXPECT_TRUE(Vector4<double>::UnitX() != Vector4<double>::UnitZ());
+	EXPECT_FALSE(Vector4<double>::UnitX() != Vector4<double>::UnitX());
 }
